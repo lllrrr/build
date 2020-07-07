71a969ed391002ea83c6c42b0e6b16738e2150b9
hong0980 <hong0980@qq.com>
2020-07-08 00:49:54 +0800

    up

--- cowbbonding/luasrc/model/cbi/cowbbonding.lua
+++ cowbbonding/luasrc/model/cbi/cowbbonding.lua
@@ -3,7 +3,6 @@ local sys = require "luci.sys"
 local wa = require "luci.tools.webadmin"
 local ipc = require "luci.ip"
 local net = require "luci.model.network".init()
-local sys = require "luci.sys"
 local ifaces = sys.net:devices()
 local button = ""
 local state_msg = ""
@@ -81,32 +80,69 @@ e:depends("work_mode", 2)
 e.rmempty = true
 
 -------------
-s = m:section(TypedSection,  "manual",  translate(""),translate("<b><font color=\"black\">手工添加 </font></b>正常填写的名单，在按下“保存并应用”后会被添加到下方编辑框名单的末尾，同时这里会被清空。无须在意有重复，会被自动过滤。其中“主机名”如不需要可以留空。"))
+s = m:section(TypedSection,  "manual",  translate(""),translate("<b><font color=\"black\">手工添加 </font></b>正常填写的名单，在按下“保存并应用”后会被添加到下方编辑框名单的末尾，同时这里会被清空。其中“主机名”、“DUID”、“IPV6后缀”如不需要可以留空。"))
 s.template = "cbi/tblsection"
 s.anonymous = true
 s.addremove = true
 
-local ip = s:option(Value, "IP",translate("IP"))
-ipc.neighbors({family = 4, dev = "br-lan"}, function(n)
-	if n.mac and n.dest then
-	 ip:value(n.dest:string(), "%s (%s)" %{ n.dest:string(), n.mac })
+name = s:option(Value, "hostname", translate("Hostname"))
+name.datatype = "hostname('strict')"
+name.rmempty  = true
+function name.write(self, section, value)
+	Value.write(self, section, value)
+	m:set(section, "dns", "1")
+end
+function name.remove(self, section)
+	Value.remove(self, section)
+	m:del(section, "dns")
+end
+
+mac = s:option(Value, "MAC", translate("<abbr title=\"Media Access Control\">MAC</abbr>-Address"))
+mac.datatype = "list(macaddr)"
+mac.rmempty  = true
+function mac.cfgvalue(self, section)
+	local val = Value.cfgvalue(self, section)
+	return ipc.checkmac(val) or val
+end
+
+ip = s:option(Value, "IP", translate("<abbr title=\"Internet Protocol Version 4\">IPv4</abbr>-Address"))
+ip.datatype = "or(ip4addr,'ignore')"
+
+duid = s:option(Value, "duid", translate("<abbr title=\"The DHCP Unique Identifier\">DUID</abbr>"))
+duid.datatype = "and(rangelength(20,36),hexstring)"
+fp = io.open("/var/hosts/odhcpd")
+if fp then
+	for line in fp:lines() do
+		local net_val, duid_val = string.match(line, "# (%S+)%s+(%S+)")
+		if duid_val then
+			duid:value(duid_val, duid_val)
+		end
+	end
+	fp:close()
+end
+
+hostid = s:option(Value, "hostid", translate("<abbr title=\"Internet Protocol Version 6\">IPv6</abbr>-Suffix (hex)"))
+sys.net.host_hints(function(m, v4, v6, name)
+	if m and v4 then
+		ip:value(v4)
+		mac:value(m, "%s (%s)" %{ m, name or v4 })
 	end
 end)
+function ip.validate(self, value, section)
+	local m = mac:formvalue(section) or ""
+	local n = name:formvalue(section) or ""
+	if value and #n == 0 and #m == 0 then
+		return nil, translate("One of hostname or mac address must be specified!")
+	end
+	return Value.validate(self, value, section)
+end
 
-local ip = s:option(Value, "MAC",translate("MAC"))
-sys.net.mac_hints(function(mac, name)
-	ip:value(mac, "%s (%s)" %{ mac, name })
-end)
-
-setport =s:option(Value,"hostname",translate("Hostname"))
-setport.description = translate("")
-setport.rmempty=true
 ------------
 
 s = m:section(TypedSection, "cowbbonding")
 s.anonymous=true
 
-o = s:option(TextValue, "/etc/cowbbonding/list.cfg", translate(""), translate("所需内容可智能识别，只需每行存在一个MAC、IP、hostname(可无)，之间由空格隔开，无须在意顺序或无用内容，如可直接复制原DHCP分配记录使用。</br>Hostname命名规范：只能由a-z A-Z 0-9和符号.-_构成，词首只能为字母，符号不能在末尾，如：Alice_PC2 。不符合规范的hostname在运行中会被丢弃。"))
+o = s:option(TextValue, "/etc/cowbbonding/list.cfg", translate(""), translate("所需内容可智能识别，无须在意先后顺序，只需每行存在一个MAC、IP、IPV6前缀(可无)、DUID(可无)、hostname(可无)，之间由空格隔开。IPV6后缀、DUID仅在定制版固件可用。</br>* Hostname命名规范：只能由a-z A-Z 0-9和符号.-_构成，词首只能为字母，符号不能在末尾，如：Alice.-_PC2 。</br>* IPV6后缀可识别格式为： :ABCD ，必须是完整4位（前加 : ），不能使用缩写法，比如 :0001不能写为 :1。</br>* DUID可识别格式为：0001000123511717fc2d307f010e (28位)或 01-02-03-04-05-06-07-08-09-10-11-12-13-14 (微软格式)。"))
 o.rows = 25
 o.wrap = "off"
 function o.cfgvalue(self, section)
@@ -130,4 +166,7 @@ io.popen("/etc/init.d/cowbbonding start")
 end
 
 return m
+
+
+
 
--- luci-app-cowbping/root/etc/cowbping.sh
+++ luci-app-cowbping/root/etc/cowbping.sh
@@ -1,31 +1,32 @@
 #!/bin/sh
 # Author: wulishui <wulishui@gmail.com>
 
-echo "-----Author: wulishui , 2020421 v1.6-----" > /tmp/log/cowbping.log
+echo "-----Author: wulishui , 2020627 v1.7-----" > /tmp/log/cowbping.log
 time=$(uci get cowbping.cowbping.time 2>/dev/null)
 delaytime=$(uci get cowbping.cowbping.delaytime 2>/dev/null)
 sleep "$delaytime"
 
 while :
 do
-
 work_mode=$(uci get cowbping.cowbping.work_mode 2>/dev/null)
 sum=$(uci get cowbping.cowbping.sum 2>/dev/null)
 address=$(uci get cowbping.cowbping.address 2>/dev/null)
 pkglost=$(uci get cowbping.cowbping.pkglost 2>/dev/null)
-sum0=0
 
 ping=`ping -c 1 "$address"|grep -o -E "([0-9]|[1-9][0-9]|100)"% | awk -F '%' '{print $1}'`
 if [ "$ping" -ge "$pkglost" ]; then
- for i in $(seq 1 ${sum})
-do
-   ping=`ping -c 1 "$address"|grep -o -E "([0-9]|[1-9][0-9]|100)"% | awk -F '%' '{print $1}'` ; [ "$ping" -ge "$pkglost" ] && sum0=`expr $sum0 + 1`
-done
+ sum0=1
+ for i in $(seq 1 $((sum-1)))
+ do
+   ping=`ping -c 1 "$address"|grep -o -E "([0-9]|[1-9][0-9]|100)"% | awk -F '%' '{print $1}'`
+   [ "$ping" -ge "$pkglost" ] && sum0=$((sum0+1))
+ done
+else
+ sum0=0
 fi
 
 if [ "$sum0" -ge "$sum" ]; then
 echo "20"$(date +"%y-%m-%d %H:%M:%S")"----断网啦!!!" >> /tmp/log/cowbping.log
-
 case "$work_mode" in
 "1")
 reboot
@@ -44,22 +45,20 @@ command=$(uci get cowbping.cowbping.command 2>/dev/null)
 eval ${command} 2>/dev/null
 ;;
 "6")
-Randommac=`dd if=/dev/urandom bs=1 count=32 2>/dev/null | md5sum | cut -b 0-12 | sed 's/\(..\)/\1:/g; s/.$//'`
-uci set network.wwan.macaddr="$Randommac" 2>/dev/null
-uci commit network
-/etc/init.d/network restart
-uci set wireless.@wifi-iface[-1].macaddr="$Randommac" 2>/dev/null
+MAC=`dd if=/dev/urandom bs=1 count=32 2>/dev/null | md5sum | cut -b 0-12 | sed 's/\(..\)/\1:/g; s/.$//'`
+uci set wireless.@wifi-iface[0].macaddr="$MAC"
 uci commit wireless
-wifi down && wifi up 2>/dev/null
+wifi down
+wifi up
 ;;
 "7")
 poweroff 2>/dev/null
 ;;
 esac
-
 fi
 
 sleep "$time"
 done
+
 
 
