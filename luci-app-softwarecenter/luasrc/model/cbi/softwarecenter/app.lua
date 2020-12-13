224f9bab65bf117d860828a8244268db3b1c718a
hong0980 <hong0980@qq.com>
2020-12-13 22:23:13 +0800

    qnap_boot

--- luci-app-softwarecenter/luasrc/model/cbi/softwarecenter/app.lua
+++ luci-app-softwarecenter/luasrc/model/cbi/softwarecenter/app.lua
@@ -2,28 +2,30 @@ m = Map("softwarecenter")
 s = m:section(TypedSection, "app", "")
 s.anonymous = true
 
-p = s:option(Flag, "deluge_install",translate("启用deluge"))
+p = s:option(Flag, "deluge_install",translate("启用deluge"), translate("Deluge是一款优秀的BT下载客户端，采用python和GTK+开发。"))
 p.default = 0
 p.rmempty = false
-p = s:option(Button, "_add", translate("安装deluge"))
-p.inputtitle = translate("开始安装")
-p.inputstyle = "apply"
-p.forcewrite = true
-function p.write(self, section)
-	luci.sys.call("cbi.apply")
-	luci.sys.call("/usr/bin/softwarecenter/lib_functions.sh ipk_install deluge deluge-ui-web &")
-	luci.sys.call("/opt/etc/init.d/S80deluged start &")
-	luci.sys.call("/opt/etc/init.d/S81deluge-web start &")
-	luci.http.redirect(luci.dispatcher.build_url("admin/services/softwarecenter/log"))
-end
-local running=(luci.sys.call("ps | grep 'deluge' | grep -v grep > /dev/null") == 0)
-local state_msg = ""
-if running then
-  state_msg = "<b><font color=\"green\">" .. translate("正在运行") .. "</font></b>"
+local state=(luci.sys.call("ps | grep 'deluge' | grep -v grep > /dev/null") == 0)
+if state then
+	o="<input class=\"cbi-button cbi-button-apply\" type=\"button\" value=\" " ..translate("打开WebUI管理").." \" onclick=\"window.open('http://'+window.location.hostname+':" .. "888" .."')\"/>"
+	state_msg = "<b><font color=\"green\">" .. translate("deluge运行中") .. "</font></b>"
 else
-  state_msg = "<b><font color=\"red\">" .. translate("没有运行") .. "</font></b>"
+	p = s:option(Button, "_add", translate("安装deluge"))
+	p.inputtitle = translate("开始安装")
+	p.inputstyle = "apply"
+	p.forcewrite = true
+	function p.write(self, section)
+		luci.sys.call("cbi.apply")
+		luci.sys.call("/usr/bin/softwarecenter/lib_functions.sh ipk_install deluge deluge-ui-web &")
+		luci.sys.call("/opt/etc/init.d/S80deluged start &")
+		luci.sys.call("/opt/etc/init.d/S81deluge-web start &")
+		luci.http.redirect(luci.dispatcher.build_url("admin/services/softwarecenter/log"))
+	end
+	state_msg = "<b><font color=\"red\">" .. translate("deluge没有运行") .. "</font></b>"
 end
-p.description = translate("deluge默认WebUI端口：888" .. "<br/>".. translate("运行状态").. " : "  .. state_msg .. "<br />")
+if state then
+	p.description = translate("deluge默认WebUI端口：888  登录密码：deluge" .. "<br/><br/>".. state_msg .. "&nbsp;&nbsp;&nbsp;".. o )
+end
 p:depends("deluge_install", 1)
 
 return m
