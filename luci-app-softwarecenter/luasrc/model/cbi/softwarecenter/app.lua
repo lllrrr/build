dcfe474e89ca591d4cbd13ae85db6ade429347a9
hong0980 <hong0980@qq.com>
2020-12-21 00:50:43 +0800

    qnap_boot

--- luci-app-softwarecenter/luasrc/model/cbi/softwarecenter/app.lua
+++ luci-app-softwarecenter/luasrc/model/cbi/softwarecenter/app.lua
@@ -3,6 +3,28 @@ m = Map("softwarecenter",translate("Entware软件安装"), translate("Entware提
 
 s = m:section(TypedSection, "ipk")
 s.anonymous = true
+-- amule
+p = s:option(Flag, "amule_install",translate("启用amule"), translate("aMule是一个开源免费的P2P文件共享软件，类似于eMule<br/>基于xMule和lMule。可应用eDonkey网络协议，也支持KAD网络。"))
+p.default = 0
+p.rmempty = false
+p = s:option(Button, "_adf", translate("安装amule"))
+p.inputtitle = translate("开始安装")
+p.inputstyle = "apply"
+p.forcewrite = true
+function p.write(self, section)
+	SYS.call("/usr/bin/softwarecenter/lib_functions.sh amule &")
+	luci.http.redirect(luci.dispatcher.build_url("admin/services/softwarecenter/log"))
+end
+local state=(SYS.call("pidof amuled > /dev/null") == 0)
+if state then
+	o="<input class=\"cbi-button cbi-button-apply\" type=\"button\" value=\" " .. translate("打开WebUI管理") .." \" onclick=\"window.open('http://'+window.location.hostname+':" .. "4711" .. "')\"/>"
+	state_msg = "<b><font color=\"green\">" .. translate("amule 已经运行") .. "</font></b>"
+	p.description = translate("amule默认WebUI端口：4711，密码：admin<br/>配置文件在   /opt/etc/config/amule.conf " .. "<br/>".. o .. "&nbsp;&nbsp;&nbsp;".. state_msg)
+else
+	state_msg = "<b><font color=\"red\">" .. translate("amule 没有运行") .. "</font></b>"
+	p.description = translate("amule默认WebUI端口：4711" .. "<br/>".. state_msg)
+end
+p:depends("amule_install", 1)
 -- aria2
 p = s:option(Flag, "aria2_install",translate("启用aria2"), translate("aria2是一款开源、轻量级的多协议命令行下载工具<br/>支持 HTTP/HTTPS、FTP、SFTP、BitTorrent 和 Metalink 协议"))
 p.default = 0
@@ -47,50 +69,6 @@ else
 	p.description = translate("deluge默认WebUI端口：888  登录密码：deluge" .. "<br/>".. state_msg)
 end
 p:depends("deluge_install", 1)
--- transmission
-p = s:option(Flag, "transmission_install",translate("启用transmission"), translate("Transmission 是一个快速、精简的 bittorrent 客户端"))
-p.default = 0
-p.rmempty = false
-p = s:option(Button, "_adc", translate("安装transmission"))
-p.inputtitle = translate("开始安装")
-p.inputstyle = "apply"
-p.forcewrite = true
-function p.write(self, section)
-	SYS.call("/usr/bin/softwarecenter/lib_functions.sh transmission &")
-	luci.http.redirect(luci.dispatcher.build_url("admin/services/softwarecenter/log"))
-end
-local state=(SYS.call("pidof transmission-daemon > /dev/null") == 0)
-if state then
-	o="<input class=\"cbi-button cbi-button-apply\" type=\"button\" value=\" " .. translate("打开WebUI管理") .." \" onclick=\"window.open('http://'+window.location.hostname+':" .. "9091" .. "')\"/>"
-	state_msg = "<b><font color=\"green\">" .. translate("transmission 已经运行") .. "</font></b>"
-	p.description = translate("transmission默认WebUI端口：9091<br/>配置文件在   /opt/etc/config/transmission.conf " .. "<br/>".. o .. "&nbsp;&nbsp;&nbsp;".. state_msg)
-else
-	state_msg = "<b><font color=\"red\">" .. translate("transmission 没有运行") .. "</font></b>"
-	p.description = translate("transmission默认WebUI端口：9091" .. "<br/>".. state_msg)
-end
-p:depends("transmission_install", 1)
--- rtorrent
-p = s:option(Flag, "rtorrent_install",translate("启用rtorrent"), translate("rtorrent是一个Linux下控制台的BT 客户端程序。"))
-p.default = 0
-p.rmempty = false
-p = s:option(Button, "_add", translate("安装rtorrent"))
-p.inputtitle = translate("开始安装")
-p.inputstyle = "apply"
-p.forcewrite = true
-function p.write(self, section)
-	SYS.call("/usr/bin/softwarecenter/lib_functions.sh rtorrent &")
-	luci.http.redirect(luci.dispatcher.build_url("admin/services/softwarecenter/log"))
-end
-local state=(SYS.call("pidof rtorrent > /dev/null") == 0)
-if state then
-	o="<input class=\"cbi-button cbi-button-apply\" type=\"button\" value=\" " .. translate("打开WebUI管理") .." \" onclick=\"window.open('http://'+window.location.hostname+':" .. "1099" .. "/rutorrent" .. "')\"/>"
-	state_msg = "<b><font color=\"green\">" .. translate("rutorrent 已经运行") .. "</font></b>"
-	p.description = translate("rutorrent默认WebUI端口：1099<br/>配置文件在   /opt/etc/config/rtorrent.conf " .. "<br/>".. o .. "&nbsp;&nbsp;&nbsp;".. state_msg)
-else
-	state_msg = "<b><font color=\"red\">" .. translate("rutorrent 没有运行") .. "</font></b>"
-	p.description = translate("rutorrent默认WebUI端口：1099" .. "<br/>".. state_msg)
-end
-p:depends("rtorrent_install", 1)
 -- qbittorrent
 p = s:option(Flag, "qbittorrent_install",translate("启用qbittorrent"), translate("qBittorrent是一个跨平台的自由BitTorrent客户端"))
 p.default = 0
@@ -113,28 +91,50 @@ else
 	p.description = translate("qbittorrent默认WebUI端口：9080" .. "<br/>".. state_msg)
 end
 p:depends("qbittorrent_install", 1)
--- amule
-p = s:option(Flag, "amule_install",translate("启用amule"), translate("aMule是一个开源免费的P2P文件共享软件，类似于eMule<br/>基于xMule和lMule。可应用eDonkey网络协议，也支持KAD网络。"))
+-- rtorrent
+p = s:option(Flag, "rtorrent_install",translate("启用rtorrent"), translate("rtorrent是一个Linux下控制台的BT 客户端程序。"))
 p.default = 0
 p.rmempty = false
-p = s:option(Button, "_adf", translate("安装amule"))
+p = s:option(Button, "_add", translate("安装rtorrent"))
 p.inputtitle = translate("开始安装")
 p.inputstyle = "apply"
 p.forcewrite = true
 function p.write(self, section)
-	SYS.call("/usr/bin/softwarecenter/lib_functions.sh amule &")
+	SYS.call("/usr/bin/softwarecenter/lib_functions.sh rtorrent &")
 	luci.http.redirect(luci.dispatcher.build_url("admin/services/softwarecenter/log"))
 end
-local state=(SYS.call("pidof amuled > /dev/null") == 0)
+local state=(SYS.call("pidof rtorrent > /dev/null") == 0)
 if state then
-	o="<input class=\"cbi-button cbi-button-apply\" type=\"button\" value=\" " .. translate("打开WebUI管理") .." \" onclick=\"window.open('http://'+window.location.hostname+':" .. "4711" .. "')\"/>"
-	state_msg = "<b><font color=\"green\">" .. translate("amule 已经运行") .. "</font></b>"
-	p.description = translate("amule默认WebUI端口：4711，密码：admin<br/>配置文件在   /opt/etc/config/amule.conf " .. "<br/>".. o .. "&nbsp;&nbsp;&nbsp;".. state_msg)
+	o="<input class=\"cbi-button cbi-button-apply\" type=\"button\" value=\" " .. translate("打开WebUI管理") .." \" onclick=\"window.open('http://'+window.location.hostname+':" .. "1099" .. "/rutorrent" .. "')\"/>"
+	state_msg = "<b><font color=\"green\">" .. translate("rutorrent 已经运行") .. "</font></b>"
+	p.description = translate("rutorrent默认WebUI端口：1099<br/>配置文件在   /opt/etc/config/rtorrent.conf " .. "<br/>".. o .. "&nbsp;&nbsp;&nbsp;".. state_msg)
 else
-	state_msg = "<b><font color=\"red\">" .. translate("amule 没有运行") .. "</font></b>"
-	p.description = translate("amule默认WebUI端口：4711" .. "<br/>".. state_msg)
+	state_msg = "<b><font color=\"red\">" .. translate("rutorrent 没有运行") .. "</font></b>"
+	p.description = translate("rutorrent默认WebUI端口：1099" .. "<br/>".. state_msg)
 end
-p:depends("amule_install", 1)
+p:depends("rtorrent_install", 1)
+-- transmission
+p = s:option(Flag, "transmission_install",translate("启用transmission"), translate("Transmission 是一个快速、精简的 bittorrent 客户端"))
+p.default = 0
+p.rmempty = false
+p = s:option(Button, "_adc", translate("安装transmission"))
+p.inputtitle = translate("开始安装")
+p.inputstyle = "apply"
+p.forcewrite = true
+function p.write(self, section)
+	SYS.call("/usr/bin/softwarecenter/lib_functions.sh transmission &")
+	luci.http.redirect(luci.dispatcher.build_url("admin/services/softwarecenter/log"))
+end
+local state=(SYS.call("pidof transmission-daemon > /dev/null") == 0)
+if state then
+	o="<input class=\"cbi-button cbi-button-apply\" type=\"button\" value=\" " .. translate("打开WebUI管理") .." \" onclick=\"window.open('http://'+window.location.hostname+':" .. "9091" .. "')\"/>"
+	state_msg = "<b><font color=\"green\">" .. translate("transmission 已经运行") .. "</font></b>"
+	p.description = translate("transmission默认WebUI端口：9091<br/>配置文件在   /opt/etc/config/transmission.conf " .. "<br/>".. o .. "&nbsp;&nbsp;&nbsp;".. state_msg)
+else
+	state_msg = "<b><font color=\"red\">" .. translate("transmission 没有运行") .. "</font></b>"
+	p.description = translate("transmission默认WebUI端口：9091" .. "<br/>".. state_msg)
+end
+p:depends("transmission_install", 1)
 -- p = s:option(Button, "_adp", translate("重启rtorrent"))
 -- p.inputtitle = translate("重启rtorrent")
 -- p.inputstyle = "apply"
