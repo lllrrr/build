4614548b8c9a4fd66ab0494fcd6d7874de2e399d
hong0980 <hong0980@qq.com>
2020-12-21 18:53:34 +0800

    qnap_boot

--- luci-app-softwarecenter/luasrc/model/cbi/softwarecenter/app.lua
+++ luci-app-softwarecenter/luasrc/model/cbi/softwarecenter/app.lua
@@ -1,9 +1,10 @@
 local SYS = require "luci.sys"
 font_green = [[<b><font color="green">]]
 font_red = [[<b><font color="red">]]
+font_off = [[</font></b>]]
 font_op = " \" onclick=\"window.open('http://'+window.location.hostname+':"
 font_apply = "<input class=\"cbi-button cbi-button-apply\" type=\"button\" value=\" "
-m = Map("softwarecenter",translate("Entware软件安装"), translate("Entware提供超过2000多个不同平台的软件包<br>配置文件都软链接在 /opt/etc/config下，以方便查看和修改"))
+m = Map("softwarecenter",translate("Entware软件安装"), translate("Entware提供超过2000多个不同平台的软件包<br>配置文件都软链接在 /opt/etc/config下，方便查看和修改"))
 
 s = m:section(TypedSection, "ipk")
 s.anonymous = true
@@ -18,11 +19,11 @@ function p.write(self, section)
 end
 local state=(SYS.call("pidof amuled > /dev/null") == 0)
 if state then
-	o=font_apply .. translate("打开WebUI管理") .. font_op.. "4711" .. "')\"/>"
-	state_msg = font_green .. translate("aMule 已经运行") .. "</font></b>"
-	p.description = translate("aMule默认WebUI端口：4711，密码：admin" .. "<br/>" .. state_msg .. "<br/>" .. o )
+	o=font_apply .. translate("打开WebUI管理") .. font_op .. "4711" .. "')\"/>"
+	state_msg = font_green .. translate("aMule 运行中") .. font_off
+	p.description = translate("aMule默认WebUI端口:4711，密码:admin" .. "<br/>" .. state_msg .. "<br/>" .. o )
 else
-	state_msg = font_red .. translate("aMule 没有运行") .. "</font></b>"
+	state_msg = font_red .. translate("aMule 没有运行") .. font_off
 	p.description = translate("aMule是一个开源免费的P2P文件共享软件，类似于eMule<br/>基于xMule和lMule。可应用eDonkey网络协议，也支持KAD网络。" .. "<br/>".. state_msg)
 end
 
@@ -37,10 +38,10 @@ end
 local state=(SYS.call("pidof aria2c > /dev/null") == 0)
 if state then
 	o=font_apply .. translate("打开AriNG管理") .." \" onclick=\"window.open('http://ariang.ghostry.cn')\"/>&nbsp;&nbsp;&nbsp;<input class=\"cbi-button cbi-button-apply\" type=\"button\" value=\" " .. translate("打开webui-aria2管理") .." \" onclick=\"window.open('http://webui-aria2.ghostry.cn')\"/>"
-	state_msg = font_green .. translate("Aria2 已经运行") .. "</font></b>"
-	p.description = translate("Aria2 RPC监听端口默认为：6800; RPC密码默认为：Passw0rd" .. "<br/>" .. state_msg .. "<br/>" .. o )
+	state_msg = font_green .. translate("Aria2 运行中") .. font_off
+	p.description = translate("Aria2 RPC监听端口默认为:6800，RPC密码默认为:Passw0rd" .. "<br/>" .. state_msg .. "<br/>" .. o )
 else
-	state_msg = font_red .. translate("Aria2 没有运行") .. "</font></b>"
+	state_msg = font_red .. translate("Aria2 没有运行") .. font_off
 	p.description = translate("Aria2 是一款开源、轻量级的多协议命令行下载工具<br/>支持 HTTP/HTTPS、FTP、SFTP、BitTorrent 和 Metalink 协议" .. "<br/>".. state_msg)
 end
 
@@ -54,11 +55,11 @@ p.write = function()
 end
 local state=(SYS.call("pidof deluge-web > /dev/null") == 0)
 if state then
-	o=font_apply .. translate("打开WebUI管理") .. font_op.. "888" .. "')\"/>"
-	state_msg = font_green .. translate("Deluge 已经运行") .. "</font></b>"
-	p.description = translate("Deluge默认WebUI端口：888  登录密码：deluge" .. "<br/>" .. state_msg .. "<br/>" .. o )
+	o=font_apply .. translate("打开WebUI管理") .. font_op .. "888" .. "')\"/>"
+	state_msg = font_green .. translate("Deluge 运行中") .. font_off
+	p.description = translate("Deluge默认WebUI端口:888，登录密码:deluge" .. "<br/>" .. state_msg .. "<br/>" .. o )
 else
-	state_msg = font_red .. translate("Deluge 没有运行") .. "</font></b>"
+	state_msg = font_red .. translate("Deluge 没有运行") .. font_off
 	p.description = translate("Deluge是一个免费好用的BT下载软件，使用libtorrent作为其后端<br/>多种用户界面，占用系统资源少，有丰富的插件来实现核心以外的众多功能。" .. "<br/>".. state_msg)
 end
 
@@ -72,11 +73,11 @@ function p.write(self, section)
 end
 local state=(SYS.call("pidof qbittorrent-nox > /dev/null") == 0)
 if state then
-	o=font_apply .. translate("打开WebUI管理") .. font_op.. "9080" .. "')\"/>"
-	state_msg = font_green .. translate("qBittorrent 已经运行") .. "</font></b>"
-	p.description = translate("qBittorrent默认WebUI端口：9080 用启名：admin 密码：adminadmin" .. "<br/>" .. state_msg .. "<br/>" .. o )
+	o=font_apply .. translate("打开WebUI管理") .. font_op .. "9080" .. "')\"/>"
+	state_msg = font_green .. translate("qBittorrent 运行中") .. font_off
+	p.description = translate("qBittorrent默认WebUI端口:9080，用启名:admin，密码:adminadmin" .. "<br/>" .. state_msg .. "<br/>" .. o )
 else
-	state_msg = font_red .. translate("qBittorrent 没有运行") .. "</font></b>"
+	state_msg = font_red .. translate("qBittorrent 没有运行") .. font_off
 	p.description = translate("qBittorrent是一个跨平台的自由BitTorrent客户端" .. "<br/>".. state_msg)
 end
 
@@ -90,11 +91,11 @@ function p.write(self, section)
 end
 local state=(SYS.call("pidof rtorrent > /dev/null") == 0)
 if state then
-	o=font_apply .. translate("打开WebUI管理") .. font_op.. "1099" .. "/rutorrent" .. "')\"/>"
-	state_msg = font_green .. translate("rTorrent 已经运行") .. "</font></b>"
-	p.description = translate("rTorrent默认WebUI端口：1099" .. "<br/>" .. state_msg .. "<br/>" .. o )
+	o=font_apply .. translate("打开WebUI管理") .. font_op .. "1099" .. "/rutorrent" .. "')\"/>"
+	state_msg = font_green .. translate("rTorrent 运行中") .. font_off
+	p.description = translate("rTorrent默认WebUI端口:1099" .. "<br/>" .. state_msg .. "<br/>" .. o )
 else
-	state_msg = font_red .. translate("rTorrent 没有运行") .. "</font></b>"
+	state_msg = font_red .. translate("rTorrent 没有运行") .. font_off
 	p.description = translate("rTorrent是一个Linux下控制台的BT 客户端程序。" .. "<br/>".. state_msg)
 end
 
@@ -108,11 +109,11 @@ function p.write(self, section)
 end
 local state=(SYS.call("pidof transmission-daemon > /dev/null") == 0)
 if state then
-	o=font_apply .. translate("打开WebUI管理") .. font_op.. "9091" .. "')\"/>"
-	state_msg = font_green .. translate("Transmission 已经运行") .. "</font></b>"
-	p.description = translate("transmission默认WebUI端口：9091" .. "<br/>" .. state_msg .. "<br/>" .. o )
+	o=font_apply .. translate("打开WebUI管理") .. font_op .. "9091" .. "')\"/>"
+	state_msg = font_green .. translate("Transmission 运行中") .. font_off
+	p.description = translate("Transmission默认WebUI端口:9091" .. "<br/>" .. state_msg .. "<br/>" .. o )
 else
-	state_msg = font_red .. translate("Transmission 没有运行") .. "</font></b>"
+	state_msg = font_red .. translate("Transmission 没有运行") .. font_off
 	p.description = translate("Transmission 是一个快速、精简的 bittorrent 客户端" .. "<br/>".. state_msg)
 end
 -- p:depends("transmission_install", 1)
