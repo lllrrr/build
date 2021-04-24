b5fdce97bdd74a9b86d2146845fc84771d9bd7cd
hong0980 <hong0980@qq.com>
2021-04-24 13:44:19 +0800

    qnap_boot

--- luci-app-axel/luasrc/controller/axel.lua
+++ luci-app-axel/luasrc/controller/axel.lua
@@ -1,23 +1,9 @@
--- Copyright 2012 Gabor Varga <vargagab@gmail.com>
--- Licensed to the public under the Apache License 2.0.
-
 module("luci.controller.axel", package.seeall)
 
 function index()
-	 if not nixio.fs.access("/etc/config/axel") then
-		 return
-	 end
-	
-	entry({"admin", "nas"}, firstchild(), "NAS", 44).dependent = false
-	-- entry({"admin","nas","axel","status"},call("act_status")).leaf=true
-
-	local page = entry({"admin", "nas", "axel"}, cbi("axel"), _("axel"))
-	page.dependent = true
-
+	local fs = require "nixio.fs"
+	if not nixio.fs.access("/etc/config/axel") then
+		return
+	end
+	entry({"admin", "nas", "axel"}, cbi("axel"), _("axel"), 20).dependent = true
 end
--- function act_status()
-  -- local e={}
-  -- e.running=luci.sys.call("pgrep axel > /dev/null") == 0
-  -- luci.http.prepare_content("application/json")
-  -- luci.http.write_json(e)
--- end
