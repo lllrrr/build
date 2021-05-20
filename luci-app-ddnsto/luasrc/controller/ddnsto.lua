c0982917e94baab760bc43b58cdae40ced05f820
hong0980 <hong0980@qq.com>
2021-05-20 20:34:48 +0800

    update@2021-05-20-20-34

--- luci-app-ddnsto/luasrc/controller/ddnsto.lua
+++ luci-app-ddnsto/luasrc/controller/ddnsto.lua
@@ -1,14 +1,10 @@
 module("luci.controller.ddnsto",package.seeall)
 function index()
 if not nixio.fs.access("/etc/config/ddnsto")then
-return
+  return
 end
-entry({"admin","services","ddnsto"},cbi("ddnsto/global"),_("DDNSTO 内网穿透"),5).dependent=true
-entry({"admin","services","ddnsto","status"},call("act_status")).leaf=true
-end
-function act_status()
-local e={}
-e.ddnsto=luci.sys.call("pgrep /usr/bin/ddnsto >/dev/null")==0
-luci.http.prepare_content("application/json")
-luci.http.write_json(e)
+entry({"admin","services","ddnsto"},
+cbi("ddnsto/global"),
+_("DDNSTO 内网穿透"),5).dependent=true
+
 end
--- luci-app-ddnsto/luasrc/model/cbi/ddnsto/global.lua
+++ luci-app-ddnsto/luasrc/model/cbi/ddnsto/global.lua
@@ -1,21 +1,19 @@
 local e=require"nixio.fs"
-local e=luci.http
-local a,t,e
-a=Map("ddnsto",translate("DDNSTO内网穿透"),
-translate("DDNSTO是koolshare小宝开发的，支持http2的快速远程穿透工具。") ..
+local state_msg = ""
+local running=(luci.sys.call("pgrep /usr/bin/ddnsto >/dev/null")==0)
+if running then
+  state_msg = "<font color=\"green\">" .. translate("已运行") .. "</font>"
+else
+  state_msg = "<font color=\"red\">" .. translate("未运行") .. "</font>"
+end
+m=Map("ddnsto",translate("DDNSTO 内网穿透"))
+m.description = translate("DDNSTO是koolshare小宝开发支持HTTP2.0快速远程穿透的工具。") .. 
 translate("<br><br><input class=\"cbi-button cbi-button-apply\" type=\"button\" value=\"" .. 
-translate("注册配置管理") ..
-" \" onclick=\"window.open('https://www.ddnsto.com/')\"/>"))
+translate("注册与教程") ..
+"\" onclick=\"window.open('https://www.ddnsto.com')\"/>") ..
+"<br><br><b>" .. translate("运行状态：") .. state_msg .. "</b>"
 
-a.template="ddnsto/index"
-t=a:section(TypedSection,"global",translate("Running Status"))
-t.anonymous=true
-
-e=t:option(DummyValue,"_status",translate("Running Status"))
-e.template="ddnsto/dvalue"
-e.value=translate("Collecting data...")
-
-t=a:section(TypedSection,"global",translate("全局设置"),translate("设置教程:</font><a style=\"color: #ff0000;\" onclick=\"window.open('http://koolshare.cn/thread-116500-1-1.html')\">点击跳转到论坛教程</a>"))
+t=m:section(TypedSection,"global",translate("设置"))
 t.anonymous=true
 t.addremove=false
 
@@ -23,13 +21,13 @@ e=t:option(Flag,"enable",translate("启用"))
 e.default=0
 e.rmempty=false
 
+e=t:option(Value,"token",translate('ddnsto 令牌'))
+e.password=true
+e.rmempty=false
+
 e=t:option(Value,"start_delay",translate("延迟启动"),translate("单位：秒"))
 e.datatype="uinteger"
 e.default="0"
 e.rmempty=true
 
-e=t:option(Value,"token",translate('ddnsto 令牌'))
-e.password=true
-e.rmempty=false
-
-return a
+return m
