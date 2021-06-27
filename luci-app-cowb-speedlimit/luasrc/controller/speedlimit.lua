36382e629d45fc39bc86ba48d99eca30349a025c
hong0980 <hong0980@qq.com>
2021-06-27 13:55:37 +0800

    update@2021-06-27-13-55

--- luci-app-cowb-speedlimit/luasrc/controller/speedlimit.lua
+++ luci-app-cowb-speedlimit/luasrc/controller/speedlimit.lua
@@ -1,9 +1,7 @@
 module("luci.controller.speedlimit", package.seeall)
 
 function index()
-    if not nixio.fs.access("/etc/config/speedlimit") then return end
+  if not nixio.fs.access("/etc/config/speedlimit") then return end
 
-    entry({"admin", "control"}, firstchild(), "Control", 50).dependent = false
-    entry({"admin", "control", "speedlimit"}, cbi("speedlimit"), _("速度限制"), 6).dependent = true
+  entry({"admin", "network", "speedlimit"}, cbi("speedlimit"), _("速度限制"), 55).dependent = true
  end
-
